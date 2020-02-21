ace160539b0750331c17c1ea0a57524254ff0da5
Sharnw <sharn.white@gmail.com>
2020-02-21 18:07:15 +1000

    Attempt to use seeder source in GameSeeder

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -6,8 +6,10 @@ use App\Game;
 use Faker\Generator as Faker;
 
 $factory->define(Game::class, function (Faker $faker) {
+	$source = App\Source::first();
     return [
         'name' => $faker->word,
-        'user_id' => App\User::first()->id
+        'user_id' => App\User::first()->id,
+        'source_id' => $source ? $source->id : null
     ];
 });
